5ff08471d728f0b25f9b6ffcab4fb6eb74867a37
lestivalet <luiz.estivalet@trt4.jus.br>
2018-04-12 16:52:32 -0300

    .

--- app.js
+++ app.js
@@ -40,4 +40,21 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url)
+.then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...');
+   // process.exit();
+});
+
+
+
 module.exports = app;
